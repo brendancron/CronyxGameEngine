1472d2791bd73fde4ba4f53eab24f190acee7f7b
Brendan Cron <brendancron@gmail.com>
2025-09-04 17:01:20 -0400

    testing for ASST injection w/n generic safety

--- Core/Cronyx/Core/Interfaces.fs
+++ Core/Cronyx/Core/Interfaces.fs
@@ -16,25 +16,25 @@ type IEffectModifier<'eff> =
     Event Triggers will spawn new logic to be executed once current effects are modified
 *)
 
-and IEventTrigger<'eff,'event> =
+and IEventTrigger<'eff,'event,'state when 'state :> IGameState<'eff,'event,'state>> =
     abstract member Id : string
     abstract member OnEvent : 'event -> IStmt<'eff,'event,'state> option
 
-and IGameState<'eff, 'event> = 
+and IGameState<'eff, 'event, 'state when 'state :> IGameState<'eff,'event,'state>> = 
     abstract member Modifiers: IEffectModifier<'eff> list
-    abstract member Triggers: IEventTrigger<'eff, 'event> list
+    abstract member Triggers: IEventTrigger<'eff,'event,'state> list
 
 // DSL
 
-and Env<'eff,'event,'state when 'state :> IGameState<'eff,'event>> = {
+and Env<'eff,'event,'state when 'state :> IGameState<'eff,'event,'state>> = {
     Scopes     : Map<string,obj> list
     GameState  : 'state
     Trace      : 'event list
     Provenance : Set<string> list
 }
 
-and IExpr<'a,'eff,'event,'state when 'state :> IGameState<'eff,'event>> =
+and IExpr<'a,'eff,'event,'state when 'state :> IGameState<'eff,'event,'state>> =
     abstract member Eval : Env<'eff,'event,'state> -> 'a
 
-and IStmt<'eff,'event,'state when 'state :> IGameState<'eff,'event>> =
+and IStmt<'eff,'event,'state when 'state :> IGameState<'eff,'event,'state>> =
     abstract member Exec : Env<'eff,'event,'state> -> Env<'eff,'event,'state>
--- Examples/SimpleScoreGame/Program.fs
+++ Examples/SimpleScoreGame/Program.fs
@@ -1,5 +1,4 @@
-﻿open Cronyx.Effects
-open Cronyx.Expressions
+﻿open Cronyx.Expressions
 open Cronyx.Statements
 open Cronyx.Core
 
@@ -17,11 +16,15 @@ type State =
     {
         PlayerMap: Map<PlayerId, int>
         Modifiers: IEffectModifier<Effect> list
-        Triggers: IEventTrigger<Effect, Event> list
+        Triggers: IEventTrigger<Effect, Event, State> list
     }
-    interface IGameState<Effect, Event> with
+    interface IGameState<Effect, Event, State> with
         member this.Modifiers = this.Modifiers
         member this.Triggers = this.Triggers
+
+type Expr<'a> = IExpr<'a,Effect,Event,State>
+type Stmt = IStmt<Effect,Event,State>
+type Env = Env<Effect,Event,State>
 
 let validate_effect (effect: Effect) (state: State) : bool =
   let playerExists pid = Map.containsKey pid state.PlayerMap
@@ -59,21 +62,39 @@ type GlobalHealingScale(id: string, percent: float) =
             | Heal (player, amt) -> Heal (player, int(float(amt) * percent))
             | other    -> other
 
-(*
-type ReflectDamage(id: string, target: string, source: string, percent: float) =
-    interface IEventTrigger<Effect, Event> with
+type HealExpr<'eff,'event,'state when 'state :> IGameState<'eff,'event,'state>>
+    (player: PlayerId, amount: IExpr<int,'eff,'event,'state>) =
+    interface IExpr<Effect,'eff,'event,'state> with
+        member _.Eval env =
+            let amt = amount.Eval env
+            Heal(player, amt)
+
+type DamageExpr<'eff,'event,'state when 'state :> IGameState<'eff,'event,'state>>
+    (player: PlayerId, amount: IExpr<int,'eff,'event,'state>) =
+    interface IExpr<Effect,'eff,'event,'state> with
+        member _.Eval env =
+            let amt = amount.Eval env
+            Damage(player, amt)
+
+type ReflectDamage(id: string, target: PlayerId, source: PlayerId, percent: float) =
+    interface IEventTrigger<Effect, Event, State> with
         member _.Id = id
-        member _.OnEvent event = 
-            match event with
-            | DamageResolved (pid, amt) when pid = source -> [Damage(target, int(float(amt) * percent))]
-            | _ -> []
-*)
+        member _.OnEvent ev =
+            match ev with
+            | DamageResolved (pid, amt) when pid = source ->
+                let reflectedAmt = int (float amt * percent)
+                Some (
+                    EffectStmt(
+                        DamageExpr(target, IntExpr reflectedAmt),
+                        validate_effect,
+                        apply_effect
+                    ) :> Stmt
+                )
+            | _ -> None
 
-// Modifier Construction
+let reflectAliceOnBob = ReflectDamage("reflect-Alice-Bob", "Alice", "Bob", 0.5)
+let reflectBobOnAlice = ReflectDamage("reflect-Bob-Alice", "Bob", "Alice", 0.5)
 
-//let siphonHeal = SiphonHeal("siphon", "Alice", "Bob", 0.25)
-//let reflectDamage = ReflectDamage("reflect", "Alice", "Bob", 0.5)
-//let reflectDamage2 = ReflectDamage("reflect2", "Bob", "Alice", 0.5)
 let doubleHeal = GlobalHealingScale("Global Heal", 2.0)
 
 // State Construction
@@ -84,45 +105,27 @@ let initialState = {
         "Bob", 100 ]
         |> Map.ofList
     Modifiers = [doubleHeal]
-    Triggers = []
+    Triggers = [reflectAliceOnBob]
 }
-
-// Expr Construction
-
-let healExpr target amt = EffectExpr(Heal(target, amt), Set.empty)
-let damageExpr target amt = EffectExpr(Damage(target, amt), Set.empty)
-
-type HealExpr<'eff,'event,'state when 'state :> IGameState<'eff,'event>>
-    (player: PlayerId, amount: IExpr<int,'eff,'event,'state>) =
-    interface IExpr<Effect,'eff,'event,'state> with
-        member _.Eval env =
-            let amt = amount.Eval env
-            Heal(player, amt)
-
-type DamageExpr<'eff,'event,'state when 'state :> IGameState<'eff,'event>>
-    (player: PlayerId, amount: IExpr<int,'eff,'event,'state>) =
-    interface IExpr<Effect,'eff,'event,'state> with
-        member _.Eval env =
-            let amt = amount.Eval env
-            Damage(player, amt)
 
 // Giga drain example
 
-let gigaDrain self target healRatio =
-    BlockStmt<Effect,Event,State>([
-        EffectStmt(DamageExpr(target, IntExpr<_,_,_>(50)), validate_effect, apply_effect) :> IStmt<_,_,_>
-        EffectStmt(HealExpr(self, 
-            MulExpr<int,float,int,Effect,Event,State>(
-                FoldEventsExpr<Event,int,Effect,State>(
+let gigaDrain self target healRatio : Stmt =
+    BlockStmt([
+        EffectStmt(DamageExpr(target, IntExpr 50), validate_effect, apply_effect) :> Stmt
+        EffectStmt(HealExpr(self,
+            MulExpr(
+                FoldEventsExpr(
                     (fun acc ev ->
                         match ev with
                         | DamageResolved(pid, amt) when pid = target -> acc + amt
                         | _ -> acc),
                     0),
-                FloatExpr<_,_,_>(healRatio),
-                (fun damage ratio -> int(float(damage) * ratio))
-            )), validate_effect, apply_effect) :> IStmt<_,_,_>
-    ]) :> IStmt<_,_,_>
+                FloatExpr healRatio,
+                (fun damage ratio -> int (float damage * ratio))
+            )
+        ), validate_effect, apply_effect) :> Stmt
+    ]) :> Stmt
 
 // Usage
 
