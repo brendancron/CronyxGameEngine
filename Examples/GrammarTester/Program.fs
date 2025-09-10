177b9c603bbdeb12d427f864fffc5c75427126eb
Brendan Cron <brendancron@gmail.com>
2025-09-09 20:33:06 -0400

    added dummy file for streamlined unit testing

--- Examples/GrammarTester/Program.fs
+++ Examples/GrammarTester/Program.fs
@@ -1,20 +1,7 @@
 ﻿open Cronyx.Core
 open Cronyx.Parsing
 open Cronyx.Parsing.Parser
-
-type Effect = unit
-type Event  = unit
-
-type State = {
-    Modifiers : IEffectModifier<Effect> list
-    Triggers  : IEventTrigger<Effect,Event,State> list
-} with
-    interface IGameState<Effect,Event,State> with
-        member this.Modifiers = this.Modifiers
-        member this.Triggers  = this.Triggers
-
-let initialState : State =
-  { Modifiers = []; Triggers = [] }
+open Cronyx.Util.Dummy
 
 let source = "3 + 4 * 5"
 
@@ -22,9 +9,9 @@ let tokens = Lexer.scan source
 
 printf "%A" (tokens)
 
-let expr, rest = parseArithmetic<Effect,Event,State> tokens
+let expr, rest = parseArithmetic<DummyEffect,DummyEvent,DummyState> tokens
 
-let env = Env.empty initialState
+let env = Env.empty emptyState
 let value : int = expr.Eval env
 
 printfn "Tokens: %A" (tokens |> List.map (fun t -> t.TokenType, t.Lexeme))
